abad41986caa56ef5ea852a5bf4c3e0773b70885
dunna Abhishek <dunnaarjun@gmail.com>
2026-02-14 18:51:41 +0530

    first commit

--- backend/controller/cartController.js
+++ backend/controller/cartController.js
@@ -84,3 +84,5 @@ export const getUserCart = async (req,res) => {
     
     
 }
+
+
--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  authDomain: "loginonecart-225ad.firebaseapp.com",
+  projectId: "loginonecart-225ad",
+  storageBucket: "loginonecart-225ad.firebasestorage.app",
+  messagingSenderId: "86320510183",
+  appId: "1:86320510183:web:49cb17aa94e8de8fb45e42"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
