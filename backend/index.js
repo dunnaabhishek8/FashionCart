11bce1310bdd7dc178d7ed439abaa25bdd640f80
dunnaabhishek8 <dunnaabhishek8@gmail.com>
2026-02-14 20:19:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://fashioncart-frontend.onrender.com" , "http://localhost:5174"],
+ origin:["https://fashioncart-frontend.onrender.com" , "https://fashioncart-admin.onrender.com"],
  credentials:true
 }))
 
